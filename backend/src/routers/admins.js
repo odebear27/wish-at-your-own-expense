f9892f8ffbb262eae9333ae3dbfb96375619de79
Odelia Tan <odelia_tan@hotmail.com>
2024-04-22 16:51:43 +0800

    Create refresh admin endpoint in admins.js

--- backend/src/routers/admins.js
+++ backend/src/routers/admins.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const { registerAdmin, loginAdmin } = require("../controllers/admins");
+const {
+  registerAdmin,
+  loginAdmin,
+  refreshAdmin,
+} = require("../controllers/admins");
 const router = express.Router();
 
 router.put("/a/register", registerAdmin);
 router.post("/a/login", loginAdmin);
+router.post("/a/refresh", refreshAdmin);
 
 module.exports = router;
