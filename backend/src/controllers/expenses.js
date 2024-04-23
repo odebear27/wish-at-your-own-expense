e44c49914bc62407cd893b41f83d3f71bde18b2d
Odelia Tan <odelia_tan@hotmail.com>
2024-04-23 14:41:24 +0800

    Create update expense for user endpoint in expenses.js

--- backend/src/controllers/expenses.js
+++ backend/src/controllers/expenses.js
@@ -59,8 +59,53 @@ const deleteExpenseForUser = async (req, res) => {
   }
 };
 
+const updateExpenseForUser = async (req, res) => {
+  try {
+    // check if user
+    if (req.decoded.role === "admin") {
+      res.json({ status: "error", msg: "unauthorised" });
+    } else if (req.decoded.role === "user") {
+      // find the expense using the expense_id
+      const { rows } = await pool.query(
+        `SELECT * FROM expenses WHERE expense_id = $1`,
+        [req.params.expense_id]
+      );
+      // check if user_id for that expense is the user that logged in
+      if (rows[0].user_id != req.decoded.id) {
+        res.json({ status: "error", msg: "unauthorised" });
+      } else if (rows[0].user_id === req.decoded.id) {
+        if ("expense_date" in req.body)
+          await pool.query(
+            `UPDATE expenses SET expense_date = $1 WHERE user_id = $2 AND expense_id=$3`,
+            [req.body.expense_date, req.decoded.id, req.params.expense_id]
+          );
+        if ("expense_item" in req.body)
+          await pool.query(
+            `UPDATE expenses SET expense_item = $1 WHERE  user_id = $2 AND expense_id=$3`,
+            [req.body.expense_item, req.decoded.id, req.params.expense_id]
+          );
+        if ("expense_category" in req.body)
+          await pool.query(
+            `UPDATE expenses SET expense_category = $1 WHERE user_id = $2 AND expense_id=$3`,
+            [req.body.expense_category, req.decoded.id, req.params.expense_id]
+          );
+        if ("expense_amt" in req.body)
+          await pool.query(
+            `UPDATE expenses SET expense_amt = $1 WHERE user_id = $2 AND expense_id=$3`,
+            [req.body.expense_amt, req.decoded.id, req.params.expense_id]
+          );
+        res.json({ status: "ok", msg: "update expense for user successful" });
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    res.json({ status: "error", msg: "update expense for user unsuccessful" });
+  }
+};
+
 module.exports = {
   getAllExpensesForUser,
   createExpenseForUser,
   deleteExpenseForUser,
+  updateExpenseForUser,
 };
--- backend/src/routers/expenses.js
+++ backend/src/routers/expenses.js
@@ -3,6 +3,7 @@ const {
   getAllExpensesForUser,
   createExpenseForUser,
   deleteExpenseForUser,
+  updateExpenseForUser,
 } = require("../controllers/expenses");
 const { authUser } = require("../middleware/auth");
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/expenses", authUser, getAllExpensesForUser);
 router.put("/expenses", authUser, createExpenseForUser);
 router.delete("/expenses/:expense_id", authUser, deleteExpenseForUser);
+router.patch("/expenses/:expense_id", authUser, updateExpenseForUser);
 
 module.exports = router;
