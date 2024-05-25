7861c967de0011a516e4ebf8eaeadf48ab618933
Odelia Tan <odelia_tan@hotmail.com>
2024-05-25 16:36:18 +0800

    Wishlist purchased will be added to expenses  from can buy modal

--- frontend/src/components/CanBuyModal.jsx
+++ frontend/src/components/CanBuyModal.jsx
@@ -1,17 +1,20 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import ReactDOM from "react-dom";
 import useFetch from "../hooks/useFetch";
 import UserContext from "../context/user";
+import ErrorMessage from "../components/ErrorMessage";
 
 const OverLay = (props) => {
   const fetchData = useFetch();
   const userCtx = useContext(UserContext);
+  const [purchasedPrice, setPurchasedPrice] = useState(props.wishlistCost);
+  const [message, setMessage] = useState("");
 
   const updateWishlistForUser = async (wishlistId) => {
     try {
       const body = {
         wishlist_item: props.wishlistItem,
-        wishlist_cost: parseFloat(props.wishlistCost),
+        wishlist_cost: parseFloat(purchasedPrice),
         wishlist_store: props.wishlistStore,
         wishlist_status: "PURCHASED",
       };
@@ -26,6 +29,68 @@ const OverLay = (props) => {
       if (res.ok) {
         props.getWishlistCost();
         props.getAllWishlistForAUser();
+      } else {
+        if (Array.isArray(res.data)) {
+          // res.data is an array
+          if (res.data.length > 0) {
+            setMessage(res.data[0]);
+          }
+        } else {
+          // or if res.data is a string
+          setMessage(res.data);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getExpenseAmt = async () => {
+    try {
+      const res = await fetchData(
+        `/api/expensesamt`,
+        "POST",
+        undefined,
+        userCtx.accessToken
+      );
+      if (res.ok) {
+        userCtx.setExpense(res.data[0].sum);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const createExpenseForUser = async () => {
+    try {
+      const body = {
+        expense_date: new Date(Date.now()).toISOString().split("T")[0],
+        expense_item: props.wishlistItem,
+        expense_category: "OTHERS",
+        expense_amt: parseFloat(purchasedPrice),
+      };
+      const res = await fetchData(
+        `/api/expenses`,
+        "PUT",
+        body,
+        userCtx.accessToken
+      );
+
+      if (res.ok) {
+        props.getWishlistCost();
+        getExpenseAmt();
+        props.setCanBuy(false);
+        props.setIsCanBuyButtonPressed(false);
+      } else {
+        if (Array.isArray(res.data)) {
+          // res.data is an array
+          if (res.data.length > 0) {
+            setMessage(res.data[0]);
+          }
+        } else {
+          // or if res.data is a string
+          setMessage(res.data);
+        }
       }
     } catch (error) {
       console.error(error);
@@ -34,19 +99,34 @@ const OverLay = (props) => {
 
   const wishlistBought = () => {
     updateWishlistForUser(props.wishlistId);
-    props.setCanBuy(false);
-    props.setIsCanBuyButtonPressed(false);
+    createExpenseForUser();
+    // props.setCanBuy(false);
+    // props.setIsCanBuyButtonPressed(false);
+  };
+
+  const handleChange = (event) => {
+    setPurchasedPrice(event.target.value);
   };
 
   return (
     <div className="z-10 w-screen h-screen bg-gray-800/75 fixed top-0 left-0 flex">
       <div className="updateModal whitesmoke px-5 z-100 fixed top-1/4 w-auto overflow-hidden py-5 rounded-md">
-        <div className="flex flex-col items-start mb-20">
+        <div className="flex flex-col items-start space-y-3 mb-20">
           <div className="font-medium">Have you purchased the item?</div>
+          <div className="w-auto flex space-x-4">
+            <label>Purchased Price:</label>
+            <input
+              id="purchasedPrice"
+              value={purchasedPrice}
+              onChange={handleChange}
+              type="text"
+            ></input>
+          </div>
+          <ErrorMessage message={message}></ErrorMessage>
         </div>
         <div className="flex justify-between">
           <button className="button" onClick={() => wishlistBought()}>
-            Yes!!! (remember to add to your expense)
+            Yes!!! (item will be added to your expenses)
           </button>
           <button
             className="button"
